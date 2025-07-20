775f511b9e8af7bd8140982c8de3c359301d7237
Italien9796 <italienlord@gmail.com>
2025-07-19 23:53:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,14 +13,13 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on",
     //chatbot on/off
   
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON-MD~",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~AVxE2RQA#ukLJvxbcPcDVMTPula84UYF2iwBAcRp0bh5W_Gt7Tog",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     // ===== OWNER & DEVELOPER SETTINGS =====
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "50948702213",  // Owner's WhatsApp number
-    
-    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "",           // Owner's name
+    OWNER_NUMBER: process.env.OWNER_NUMBER || " 34060334
+    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "sperme",           // Owner's name
     
     DEV: process.env.DEV || "50948336180",                     // Developer's contact number
     
